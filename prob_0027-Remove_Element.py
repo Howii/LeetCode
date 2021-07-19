ed02bbff620bdd0ff83b4eaf80feaf419aa08dd1
Howard Xu <howardxhs@gmail.com>
2021-07-19 12:54:54 -0400

    add prob 27

---
+++ prob_0027-Remove_Element.py
@@ -0,0 +1,7 @@
+def removeElement(nums: list, val: int) -> int:
+    i = 0
+    for j in range(len(nums)):
+        if nums[j] != val:
+            nums[i] = nums[j]
+            i += 1
+    return i
